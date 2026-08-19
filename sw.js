ded48413aa08b5112ec26bd6808f997be119d753
rgestallosinson <chuaalven418@gmail.com>
2026-08-19 18:41:33 +0800

    Reorganize Settings into collapsible accordion sections

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bscs2c-treasurer-v23-recording-date';
+const CACHE = 'bscs2c-treasurer-v24-settings-accordion';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
