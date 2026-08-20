1514e3e0460fea3c3bc1ef1915cd5e3c8037495d
rgestallosinson <chuaalven418@gmail.com>
2026-08-20 15:55:35 +0800

    Contain data tables to their own horizontal scroll box (fix mobile page widening on Expenses etc.)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bscs2c-treasurer-v32-recent-list';
+const CACHE = 'bscs2c-treasurer-v33-table-contain';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
